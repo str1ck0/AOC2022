8ef2145abf1e7568fda37ebe69dd6b924c17e19e
Liam Strickland <liam.strickland96@gmail.com>
2022-12-01 17:25:20 +0200

    Solved challenge 2

--- Day 1: Calorie Counting/day_1_calorie_counting.rb
+++ Day 1: Calorie Counting/day_1_calorie_counting.rb
@@ -1,13 +1,16 @@
 input = File.read("input.txt")
 
-puts input
-puts "------------break------------"
-p input.split("\n")
-puts "------------break------------"
-p input.split("\n\n")
-puts "------------break------------"
-p input.split("\n\n").map {_1.split("\n")}
-puts "------------break------------"
-p input.split("\n\n").map {_1.split("\n").map(&:to_i)}
-puts "------------break------------"
-puts input.split("\n\n").map {_1.split("\n").map(&:to_i).sum}.max
+# puts input
+# puts "------------break------------"
+# p input.split("\n")
+# puts "------------break------------"
+# p input.split("\n\n")
+# puts "------------break------------"
+# p input.split("\n\n").map {_1.split("\n")}
+# puts "------------break------------"
+# p input.split("\n\n").map {_1.split("\n").map(&:to_i)}
+# puts "------------break------------"
+# puts input.split("\n\n").map {_1.split("\n").map(&:to_i).sum}.max
+
+
+puts input.split("\n\n").map {_1.split("\n").map(&:to_i).sum}.max(3).sum
