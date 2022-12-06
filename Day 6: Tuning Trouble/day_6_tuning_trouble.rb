06e6fec74ec5504cb24335f135f35d0280779571
Liam Strickland <liam.strickland96@gmail.com>
2022-12-06 14:26:48 +0200

    Solved Day 6

---
+++ Day 6: Tuning Trouble/day_6_tuning_trouble.rb
@@ -0,0 +1,31 @@
+data = File.read("input.txt")
+
+characters = data.split("")
+
+array = []
+
+# PART 1
+
+# characters.each_with_index do |char, index|
+#   # if there are 4 chars in array, remove the first char
+#   array.shift if array.size == 4
+#   # add char to array
+#   array << char
+#   # if array has 4 chars and is unique, p last char's index + 1
+#   if array.size == 4 && array.uniq == array
+#     p index + 1
+#   end
+# end
+
+# PART 2
+
+characters.each_with_index do |char, index|
+  # if there are 14 chars in array, remove the first char
+  array.shift if array.size == 14
+  # add char to array
+  array << char
+  # if array has 14 chars and is unique, p last char's index + 1
+  if array.size == 14 && array.uniq == array
+    p index + 1
+  end
+end
