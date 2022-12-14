34c3c35ff7d6f92dfd8fc148a3b38045ecbd4c7d
Liam Strickland <liam.strickland96@gmail.com>
2022-12-14 11:44:21 +0800

    unsure

--- Day 6: Tuning Trouble/day_6_tuning_trouble.rb
+++ Day 6: Tuning Trouble/day_6_tuning_trouble.rb
@@ -6,16 +6,16 @@ array = []
 
 # PART 1
 
-# characters.each_with_index do |char, index|
-#   # if there are 4 chars in array, remove the first char
-#   array.shift if array.size == 4
-#   # add char to array
-#   array << char
-#   # if array has 4 chars and is unique, p last char's index + 1
-#   if array.size == 4 && array.uniq == array
-#     p index + 1
-#   end
-# end
+characters.each_with_index do |char, index|
+  # if there are 4 chars in array, remove the first char
+  array.shift if array.size == 4
+  # add char to array
+  array << char
+  # if array has 4 chars and is unique, p last char's index + 1
+  if array.size == 4 && array.uniq == array
+    p index + 1
+  end
+end
 
 # PART 2
 
